39ceeb7754e29aaa2b0249dd1c9f9e90f2b5c600
JeanSimas <jeansimas12@hotmail.com>
2022-05-24 00:51:15 -0300

    Updated data path on GetDataFromJsonFile class

--- src/functions/getDataFromJsonFile/getDataFromJsonFile.ts
+++ src/functions/getDataFromJsonFile/getDataFromJsonFile.ts
@@ -8,8 +8,14 @@ interface GetDataFromJsonFileDTO {
 export class GetDataFromJsonFile {
   constructor() {}
   execute({ filename }: GetDataFromJsonFileDTO): String | null {
-    const filePath = path.join(__dirname, "..", "data", filename + ".json");
-    //   if (!fs.existsSync(filePath)) throw new Error("Directory not found");
+    const filePath = path.join(
+      __dirname,
+      "..",
+      "..",
+      "data",
+      filename + ".json"
+    );
+    if (!fs.existsSync(filePath)) throw new Error("Directory not found");
     const dataRead = fs.readFileSync(filePath).toString();
     return dataRead;
   }
